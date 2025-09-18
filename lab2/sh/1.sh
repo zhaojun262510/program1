cd4dc72022642d7d7ef77b87f97eb79e68a4410e
zhaojun <1841681662@qq.com>
2025-09-18 14:22:07 +0800

    添加实验报告文件

---
+++ lab2/sh/1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# 编写hello world脚本
+
+echo "Hello World!"
---
+++ lab2/sh/2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+num=$[RANDOM%100+1]
+echo "$num"
+while  :
+do
+  read -p "计算机生成了一个 1‐100 的随机数,你猜: " cai
+    if [ $cai -eq $num ]
+    then
+         echo "恭喜,猜对了"
+         exit
+      elif [ $cai -gt $num ]
+      then
+             echo "Oops,猜大了"
+        else
+             echo "Oops,猜小了"
+   fi
+done
